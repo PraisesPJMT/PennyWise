c028fa34b00aca93eb2d4955ff7f2b50bff9d530
PraisesPJMT <praisesmusa@outlook.com>
2023-07-11 19:06:41 +0100

    add `User` controller

---
+++ server/controllers/user.js
@@ -0,0 +1,88 @@
+const express = require('express');
+
+const { User } = require('../models');
+const router = express.Router();
+
+// Get all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+
+    res
+      .status(200)
+      .json({ data: users, message: 'Users retrieved successfully!' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get a users
+router.get('/:user_id', async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    const user = await User.findOne({ where: { user_id } });
+
+    if (!user) {
+      res.status(404).json({ message: 'User does not exist!' });
+    }
+
+    res
+      .status(200)
+      .json({ data: user, message: 'User retrieved successfully!' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update user
+router.put('/:user_id', async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const user = await User.findOne({ where: { user_id } });
+
+    if (!user) {
+      res.status(404).json({ message: 'User does not exist!' });
+    }
+
+    const {
+      first_name,
+      last_name,
+      email,
+      funds,
+      show_funds,
+      comput_funds,
+      currency,
+    } = req.body;
+
+    if (email && user.email !== email) {
+      const emailUser = await User.findOne({ where: { email } });
+
+      if (emailUser) {
+        res.status(409).json({ message: 'User with email address exist!' });
+      } else {
+        user.email = email;
+      }
+    }
+
+    if (first_name) user.first_name = first_name;
+    if (last_name) user.last_name = last_name;
+    if (funds) user.funds = funds;
+    if (show_funds) user.show_funds = show_funds;
+    if (comput_funds) user.comput_funds = comput_funds;
+    if (currency) user.currency = currency;
+
+    const updatedUser = await user.save();
+
+    res
+      .status(200)
+      .json({ data: updatedUser, message: 'User updated successfully!' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 // Dependences
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
-// Constants
-const PORT = process.env.DB_PORT || 5005;
+// App Manager (ORM)
+const { sequelize } = require('./models');
 
-// Server Application
+// Constants
+const PORT = process.env.DB_PORT || 5005; // Localhost Port
+
+// Server Application Instance
 const app = new express();
 
 // App configuration
 app.use(cors());
 app.use(express.json());
 
+// ROUTES //
+// Session Route
+app.use('/session', require('./controllers/sessions'));
+
+// User Route
+app.use('/users', require('./controllers/user'));
+
 // Running Server
-app.listen({ port: PORT }, () => {
+app.listen({ port: PORT }, async () => {
+  // await sequelize.authenticate();
+  // await sequelize.sync({ force: true });
+  await sequelize.sync();
   console.log(`Listening on port ${PORT}`);
 });
