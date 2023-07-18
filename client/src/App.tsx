4844516d6706de0c16d03123d803c4d1ceb9563e
PraisesPJMT <praisesmusa@outlook.com>
2023-07-18 21:01:04 +0100

    configure app theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
-import AppTheme from './theme/theme';
-import { Button, Box } from '@mui/material';
+import { AppThemeProvider, useThemeContext } from './theme/theme';
+import { Box, CssBaseline } from '@mui/material';
 import './App.css';
 
 const App: React.FC<{}> = () => {
+  const { mode } = useThemeContext();
+
   return (
-    <AppTheme>
-      <Box style={{ height: '100vh', background: '#555' }}>
-        <h1>PennyWize</h1>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+    <AppThemeProvider>
+      <CssBaseline />
+      <Box
+        style={{
+          height: '100vh',
+        }}
+      >
+        <h1>PennyWize Mode: {mode}</h1>
+        <br />
       </Box>
-    </AppTheme>
+    </AppThemeProvider>
   );
 };
 
--- client/src/theme/theme.tsx
+++ client/src/theme/theme.tsx
@@ -1,58 +1,73 @@
-import { useState, FC, ReactNode, createContext, useCallback } from 'react';
+import {
+  createContext,
+  useContext,
+  useMemo,
+  useCallback,
+  ReactNode,
+  FC,
+  useState,
+} from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
 import { PaletteMode } from '@mui/material';
+import { green, purple } from '@mui/material/colors';
 
-interface ThemeContext {
-  mode: string;
-  toggleMode?: () => void;
+export type ThemeContextType = {
+  mode: PaletteMode;
+  toggleMode: () => void;
+};
+
+export interface CustomTheme {
+  palette: {
+    mode: PaletteMode;
+    primary: {
+      main: string;
+    };
+    secondary: {
+      main: string;
+    };
+  };
 }
 
-type AppThemeType = {
+const ThemeContext = createContext<ThemeContextType>({
+  mode: 'dark',
+  toggleMode: () => {},
+});
+
+export const useThemeContext = () => useContext(ThemeContext);
+
+interface AppThemeProviderProps {
   children: ReactNode;
-};
+}
 
-type DefaultModeType = {
-  mode: 'light' | 'dark';
-};
+export const AppThemeProvider: FC<AppThemeProviderProps> = ({ children }) => {
+  const [mode, setMode] = useState<PaletteMode>('dark');
 
-const defaultMode: DefaultModeType = {
-  mode: 'light',
-};
+  const toggleMode = useCallback(
+    () => setMode((prev: PaletteMode) => (prev === 'dark' ? 'light' : 'dark')),
+    []
+  );
 
-const ThemeContext = createContext<ThemeContext>(defaultMode);
-
-const AppTheme: FC<AppThemeType> = ({ children }) => {
-  const [mode, setMode] = useState<PaletteMode>(defaultMode.mode);
-
-  const toggleMode = useCallback(() => {
-    setMode((prev) => (prev === 'light' ? 'dark' : 'light'));
-  }, []);
-
-  const theme = createTheme({
-    palette: {
-      mode,
-      primary: {
-        main: green[500],
-      },
-      secondary: {
-        main: purple[500],
-      },
-    },
-  });
+  const theme: CustomTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            main: purple[500],
+          },
+          secondary: {
+            main: green[500],
+          },
+        },
+      }),
+    [mode]
+  );
 
   return (
     <ThemeProvider theme={theme}>
-      <ThemeContext.Provider
-        value={{
-          mode,
-          toggleMode: toggleMode,
-        }}
-      >
+      <ThemeContext.Provider value={{ mode, toggleMode }}>
         {children}
       </ThemeContext.Provider>
     </ThemeProvider>
   );
 };
-
-export default AppTheme;
